32318b0601041e67dffc730830463954c4b0d3e1
Blessings Chisanga <bchisanga709@gmail.com>
2025-06-12 17:14:29 +0200

    First commit

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license details. */
+
 
 /* appearance */
 static const unsigned int refresh_rate        = 60;  /* matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions */
@@ -7,22 +7,27 @@ static const unsigned int borderpx            = 1;   /* border pixel of windows 
 static const unsigned int snap                = 26;  /* snap pixel */
 static const int swallowfloating              = 1;   /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning      = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int gappih              = 10;
+static const unsigned int gappiv              = 10;
+static const unsigned int gappbh              = 10;
+static const unsigned int gapprh              = 10;
+
 static const unsigned int systrayonleft       = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing      = 5;   /* systray spacing */
 static const int systraypinningfailfirst      = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor */
 static const int showsystray                  = 1;   /* 0 means no systray */
 static const int showbar                      = 1;   /* 0 means no bar */
 static const int topbar                       = 1;   /* 0 means bottom bar */
-#define ICONSIZE                              17     /* icon size */
+#define ICONSIZE                              20     /* icon size */
 #define ICONSPACING                           5      /* space between icon and title */
 #define SHOWWINICON                           1      /* 0 means no winicon */
-static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=16", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
-static const char normbordercolor[]           = "#3B4252";
-static const char normbgcolor[]               = "#2E3440";
-static const char normfgcolor[]               = "#D8DEE9";
-static const char selbordercolor[]            = "#434C5E";
-static const char selbgcolor[]                = "#434C5E";
-static const char selfgcolor[]                = "#ECEFF4";
+static const char *fonts[]                    = { "FiraCode Nerd Font:bold:size=8"};
+static const char normbordercolor[]           = "#bd93f9";
+static const char normbgcolor[]               = "#282a36";
+static const char normfgcolor[]               = "#f8f8f2";
+static const char selbordercolor[]            = "#f5881b";
+static const char selbgcolor[]                = "#bd93f9";
+static const char selfgcolor[]                = "#282a36";
 
 static const char *colors[][3] = {
     /*               fg           bg           border   */
@@ -34,19 +39,14 @@ static const char *const autostart[] = {
     "xset", "s", "off", NULL,
     "xset", "s", "noblank", NULL,
     "xset", "-dpms", NULL,
-    "dbus-update-activation-environment", "--systemd", "--all", NULL,
-    "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
-    "flameshot", NULL,
-    "dunst", NULL,
-    "picom", "-b", NULL,
-    "sh", "-c", "feh --randomize --bg-fill ~/Pictures/backgrounds/*", NULL,
-    "/opt/Synergy/synergy-service", NULL,
-    "slstatus", NULL,
     NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "󰊖", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *rofi[]  = { "rofi", "-show", "drun", NULL };
+
+
 
 static const char ptagf[] = "[%s %s]";  /* format of a tag label */
 static const char etagf[] = "[%s]";     /* format of an empty tag */
@@ -54,19 +54,12 @@ static const int lcaselbl = 0;          /* 1 means make tag label lowercase */
 
 static const Rule rules[] = {
     /* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-    { "St",                 NULL,     NULL,           0,         0,          1,          0,         0 },
-    { "kitty",              NULL,     NULL,           0,         0,          1,          0,         0 },
     { "alacritty",          NULL,     NULL,           0,         0,          1,          0,         0 },
-    { "warp-terminal",      NULL,     NULL,           0,         0,          1,          0,         0 },
-    { "terminator",         NULL,     NULL,           0,         0,          1,          0,         0 },
-    { "lutris",             NULL,     NULL,           0,         1,          0,          0,         0 },
-    { "steam_app_default",  NULL,     NULL,           0,         1,          0,          0,         0 },
-    { "thunar",             NULL,     NULL,           0,         1,          0,          0,         0 },
     { NULL,                 NULL,     "Event Tester", 0,         0,          0,          1,        -1 }, /* xev */
 };
 
 /* layout(s) */
-static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -79,7 +72,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
     { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -91,32 +84,29 @@ static const Layout layouts[] = {
 #define STATUSBAR "dwmblocks"
 /* commands */
 static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]     = { "warp-terminal", NULL };
+static const char *termcmd[]     = { "alacritty", NULL };
 
 static Key keys[] = {
     /* modifier                     key                        function        argument */
+    { MODKEY,                       XK_d,                      spawn,          {.v = rofi } },    
     { MODKEY,                       XK_r,                      spawn,          {.v = launchercmd} },
-    { MODKEY|ControlMask,           XK_r,                      spawn,          SHCMD ("protonrestart")},
     { MODKEY,                       XK_x,                      spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,                      spawn,          SHCMD ("xdg-open https://")},
-    { MODKEY,                       XK_p,                      spawn,          SHCMD ("flameshot full -p /media/drive/Screenshots/")},
-    { MODKEY|ShiftMask,             XK_p,                      spawn,          SHCMD ("flameshot gui -p /media/drive/Screenshots/")},
+    { MODKEY,                       XK_p,                      spawn,          SHCMD ("flameshot full -p ~/Pictures/Screenshots/")},
+    { MODKEY|ShiftMask,             XK_p,                      spawn,          SHCMD ("flameshot gui -p ~/Pictures/Screenshots/")},
     { MODKEY|ControlMask,           XK_p,                      spawn,          SHCMD ("flameshot gui --clipboard")},
     { MODKEY,                       XK_e,                      spawn,          SHCMD ("xdg-open .")},
-    { MODKEY,                       XK_w,                      spawn,          SHCMD ("looking-glass-client -F")},
     { MODKEY|ShiftMask,             XK_w,                      spawn,          SHCMD ("feh --randomize --bg-fill ~/Pictures/backgrounds/*")},
     { 0,                            XF86XK_MonBrightnessUp,    spawn,          SHCMD ("xbacklight -inc 10")},
     { 0,                            XF86XK_MonBrightnessDown,  spawn,          SHCMD ("xbacklight -dec 10")},
-    { 0,                            XF86XK_AudioLowerVolume,   spawn,          SHCMD ("amixer sset Master 5%- unmute")},
-    { 0,                            XF86XK_AudioMute,          spawn,          SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")},
-    { 0,                            XF86XK_AudioRaiseVolume,   spawn,          SHCMD ("amixer sset Master 5%+ unmute")},
+    { 0,                            XK_F9,                     spawn,          SHCMD ("pactl set-sink-volume @DEFAULT_SINK@ +5%")},             
+    { 0,                            XK_F8,                     spawn,          SHCMD ("pactl set-sink-volume @DEFAULT_SINK@ -5%")},
     { MODKEY|ShiftMask,             XK_b,                      togglebar,      {0} },
     { MODKEY,                       XK_j,                      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,                      focusstack,     {.i = -1 } },
     { MODKEY|ShiftMask,             XK_j,                      movestack,      {.i = +1 } },
     { MODKEY|ShiftMask,             XK_k,                      movestack,      {.i = -1 } },
     { MODKEY,                       XK_i,                      incnmaster,     {.i = +1 } },
-    { MODKEY,                       XK_d,                      incnmaster,     {.i = -1 } },
+    //{ MODKEY,                       XK_d,                      incnmaster,     {.i = -1 } },
     { MODKEY,                       XK_h,                      setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_l,                      setmfact,       {.f = +0.05} },
     { MODKEY|ShiftMask,             XK_h,                      setcfact,       {.f = +0.25} },
@@ -142,7 +132,6 @@ static Key keys[] = {
     TAGKEYS(                        XK_4,                      3)
     TAGKEYS(                        XK_5,                      4)
     { MODKEY|ShiftMask,             XK_q,                      quit,           {0} },
-    { MODKEY|ControlMask,           XK_q,                      spawn,          SHCMD("$HOME/.config/rofi/powermenu.sh")},
     { MODKEY|ControlMask|ShiftMask, XK_r,                      spawn,          SHCMD("systemctl reboot")},
     { MODKEY|ControlMask|ShiftMask, XK_s,                      spawn,          SHCMD("systemctl suspend")},
 };
